6438064377e6cdd84ff97623f48ee065e3a4c30b
a6b8 <andreasbanholzer@me.com>
2021-05-26 12:42:35 +0200

    v0.61

--- index.rb
+++ index.rb
@@ -53,7 +53,7 @@ class Discover < Sinatra::Base
     set :bind, '0.0.0.0'
     set :port, '80'
 
-    #secrets = set_secrets()
+    # secrets = set_secrets()
     secrets = {
         debug: true
     }
